6fd644d186227a7acee16405e000e903d64100d5
USER <USER@16.24.14.137>
2016-03-16 11:37:31 +0100

    Added config.properties at WEB-INF

--- IssueRetriever/src/config/LoadConfig.java
+++ IssueRetriever/src/config/LoadConfig.java
@@ -4,33 +4,71 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
 public class LoadConfig {
 
 	private static final String PROPERTY_DATEFORMAT = "date_format";
 	private static final String PROPERTY_ENDPOINT = "gitlab_endpoint";
 	private static final String PROPERTY_TOKEN = "gitlab_token";
 	private static String DATE_FORMAT = "dd/MM/yyyy";
-	private String GITLAB_URL;
-	private String GITLAB_TOKEN;
+	private static String GITLAB_URL;
+	private static String GITLAB_TOKEN;
 	private static LoadConfig config;
 	static Properties prop;
 
+	private static String errorMessage;
+	private static boolean error = false;
+	
 	static {
 		prop = new Properties();
-		try (InputStream inputStream = LoadConfig.class.getClassLoader().getResourceAsStream("config.properties")) {
+		ServletContext servletContext = (ServletContext) FacesContext
+			    .getCurrentInstance().getExternalContext().getContext();
+		try (InputStream inputStream = servletContext.getResourceAsStream("/WEB-INF/config.properties")) {
 
 			prop.load(inputStream);
+
+			DATE_FORMAT = prop.getProperty(PROPERTY_DATEFORMAT);
+			if (DATE_FORMAT == null || "".equals(DATE_FORMAT)) {
+				error = true;
+				errorMessage = "Missing date forma configuration";
+			}
+
+			GITLAB_TOKEN = prop.getProperty(PROPERTY_TOKEN);
+			if (GITLAB_TOKEN == null || "".equals(GITLAB_TOKEN)) {
+				error = true;
+				errorMessage = "Missing Gitlab authentication token configuration";
+			}
+
+			GITLAB_URL = prop.getProperty(PROPERTY_ENDPOINT);
+			if (GITLAB_URL == null || "".equals(GITLAB_URL)) {
+				error = true;
+				errorMessage = "Missing Gitlab endpoint configuration";
+			}
 
 		} catch (IOException e) {
 			// log.severe("Unable to load properties: " + e.getMessage());
 		}
 	}
 
+	public static boolean isError() {
+		return error;
+	}
+
+	public static void setError(boolean error) {
+		LoadConfig.error = error;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
 	public static LoadConfig getConfig() {
 		if (config == null) {
 			config = new LoadConfig();
 		}
-		
+
 		return config;
 	}
 
