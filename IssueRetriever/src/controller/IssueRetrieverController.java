dd151bc3275a00951be590098950ac18c43fc69c
USER <USER@16.24.14.137>
2016-03-16 11:03:04 +0100

    Added refresh method

--- IssueRetriever/src/controller/IssueRetrieverController.java
+++ IssueRetriever/src/controller/IssueRetrieverController.java
@@ -17,21 +17,32 @@ public class IssueRetrieverController {
 	@EJB
 	private IssueRetrieverLocal issueRetriever;
 
+	private List<GitlabProject> projects;
+	
 	public List<GitlabProject> getProjects() {
 		try {
-			long initTime = System.currentTimeMillis();
-			List<GitlabProject> results = issueRetriever.getProjects();
-			System.out.println("PROJECTS: " + results.size() + ". TIME: "
-					+ (float) (System.currentTimeMillis() - initTime) / 1000 + " seconds");
-			for (GitlabProject project : results) {
-				System.out.println(project.getNameWithNamespace() + " : " + project.getTagList());
+			
+			if(projects ==null){
+				long initTime = System.currentTimeMillis();
+				projects = issueRetriever.getProjects();
+				System.out.println("PROJECTS: " + projects.size() + ". TIME: "
+						+ (float) (System.currentTimeMillis() - initTime) / 1000 + " seconds");
+//				for (GitlabProject project : results) {
+//					System.out.println(project.getNameWithNamespace() + " : " + project.getTagList());
+//				}
 			}
-			return results;
+			
+			return projects;
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.out.println("ERROR: " + ex.getMessage());
 			return null;
 		}
+	}
+	
+	
+	public void refresh(){
+		projects = null;
 	}
 }
