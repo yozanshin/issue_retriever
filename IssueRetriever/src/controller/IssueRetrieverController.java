2114192ef997a66efad646ea2e639d11142c4ac0
Manuel <Manuel@16.24.14.149>
2016-03-17 10:51:09 +0100

    Index & controller V1.1

--- IssueRetriever/src/controller/IssueRetrieverController.java
+++ IssueRetriever/src/controller/IssueRetrieverController.java
@@ -1,16 +1,23 @@
 package controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.gitlab.api.models.GitlabLabel;
 import org.gitlab.api.models.GitlabMilestone;
 import org.gitlab.api.models.GitlabProject;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
 
+import config.LoadConfig;
 import logic.IssueRetrieverLocal;
 
 @ManagedBean
@@ -22,9 +29,16 @@ public class IssueRetrieverController {
 
 	private List<GitlabProject> projects;
 	private List<GitlabLabel> labels;
+	private List<GitlabMilestone> milestones;
 
-	private GitlabProject project;
-	private GitlabLabel label;
+	private String project;
+	private String label1;
+	private String label2;
+	private String label3;
+	private String milestone;
+	
+	private Date startDate;
+	private Date endDate;
 
 	public List<GitlabProject> getProjects() {
 		try {
@@ -50,18 +64,23 @@ public class IssueRetrieverController {
 	}
 
 	public void onChange() {
-		if (project != null && !project.equals(""))
-			labels = getLabels(project.getId());
-		else
-			labels = new ArrayList<GitlabLabel>();
+		if (project != null && !project.equals("")){
+			setLabels(getLabels(Integer.parseInt(project)));
+			setMilestones(getMilestones(Integer.parseInt(project)));
+		}
+		else{
+			setLabels(new ArrayList<GitlabLabel>());
+		setMilestones(new ArrayList<GitlabMilestone>());
+		}
 	}
 
-	public GitlabProject getProject() {
+	public String getProject() {
 		return project;
 	}
 
-	public void setProject(GitlabProject project) {
+	public void setProject(String project) {
 		this.project = project;
+		onChange();
 	}
 
 	public List<GitlabLabel> getLabels(Integer projectId) {
@@ -74,20 +93,40 @@ public class IssueRetrieverController {
 		return new ArrayList<GitlabLabel>();
 	}
 
-	public List<GitlabMilestone> getMilestones(String projectId) throws Exception {
-		return issueRetriever.getMilestones(projectId);
+	public List<GitlabMilestone> getMilestones(Integer projectId){
+		try {
+		return issueRetriever.getMilestones(projectId.toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new ArrayList<GitlabMilestone>();
 	}
 
-	public GitlabLabel getLabel() {
-		return label;
+	public String getLabel1() {
+		return label1;
 	}
 
-	public void setLabel(GitlabLabel label) {
-		this.label = label;
+	public void setLabel(String label1) {
+		this.label1 = label1;
 	}
 
-	
-	
+	public String getLabel2() {
+		return label2;
+	}
+
+	public void setLabel2(String label2) {
+		this.label2 = label2;
+	}
+
+	public String getLabel3() {
+		return label3;
+	}
+
+	public void setLabel3(String label3) {
+		this.label3 = label3;
+	}
+
 	public List<GitlabLabel> getLabels() {
 		return labels;
 	}
@@ -95,8 +134,53 @@ public class IssueRetrieverController {
 	public void setLabels(List<GitlabLabel> labels) {
 		this.labels = labels;
 	}
+	
+	public String getMilestone() {
+		return milestone;
+	}
 
+	public void setMilestone(String milestone) {
+		this.milestone = milestone;
+	}
+	
 	public void refresh() {
 		projects = null;
+	}
+
+	public List<GitlabMilestone> getMilestones() {
+		return milestones;
+	}
+
+	public void setMilestones(List<GitlabMilestone> milestones) {
+		this.milestones = milestones;
+	}
+
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat(LoadConfig.getInstance().getDateFormat());
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+     
+    public void click() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+         
+        requestContext.update("form:display");
+        requestContext.execute("PF('dlg').show()");
+    }
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
 	}
 }
